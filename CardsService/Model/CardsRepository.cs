8fbef731d7e40d6adbac84fe63809a5066af9ea6
Darek Kacban <darek.kacban@gmail.com>
2017-08-14 21:55:22 +0200

    remore unused namespaces

--- CardsService/Model/CardsRepository.cs
+++ CardsService/Model/CardsRepository.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Octokit;
-using Octokit.Internal;
 
 namespace CardsService.Model
 {
